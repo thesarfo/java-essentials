5f5ca91bb811fc554baf0d25b969aec5e4ba1440
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-13 15:15:49 +0000

    add corresponding programs

--- programs/Main.java
+++ programs/Main.java
@@ -1,10 +1,24 @@
 package programs;
 
-class Main {
-    public static void main(String[] args){
-        int age;
-        age = 21;
+//class Main {
+//    public static void main(String[] args){
+//        int age;
+//        age = 21;
+//
+//        System.out.println("I am " + age + " years old");
+//    }
+//}
 
-        System.out.println("I am " + age + " years old");
+class Main{
+    public static void main(String[] args){
+        // implicit type conversion, the compiler does it for us
+//        int number1 = 5;
+//        double number2 = number1;
+
+        //explcit type conversion
+        double number1 = 5.8;
+        int number2 = (int)number1;
+
+        System.out.println(number2);
     }
 }
