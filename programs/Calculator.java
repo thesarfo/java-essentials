71c8d28ea2762234fb96947a2ba89648b8a9ae52
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-13 19:18:58 +0000

    calculator program

---
+++ programs/Calculator.java
@@ -0,0 +1,38 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter the first number: ");
+        double number1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("enter the second number: ");
+        double number2 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("What operation do you want to perform?: ");
+        String operation = scanner.nextLine();
+
+        if ( operation.equals("sum")){
+            System.out.printf("%f + %f = %f", number1, number2, number1+number2 );
+        } else if (operation.equals("sub")) {
+            System.out.printf("%f - %f = %f", number1, number2, number1-number2);
+        } else if (operation.equals("mul")) {
+            System.out.printf("%f * %f = %f", number1, number2, number1*number2);
+        } else if (operation.equals("div")) {
+            if (number2 == 0){
+                System.out.println("Cannot divide by zero");
+            } else {
+                System.out.printf("%f * %f = %f", number1, number2, number1/number2);
+            }
+        } else {
+            System.out.printf("%s is not a supported operation.", operation);
+        }
+
+        scanner.close();
+    }
+}
