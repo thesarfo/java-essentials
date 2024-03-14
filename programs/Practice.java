f65d469dd7c22e82c81fe0fa9fc56bd30020c362
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-14 16:45:08 +0000

    practice program

---
+++ programs/Practice.java
@@ -0,0 +1,12 @@
+package programs;
+
+public class Practice {
+    public static void main(String[] args){
+//        int x = 1;
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
+
+        System.out.println(y);
+
+    }
+}
