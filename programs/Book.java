890bf86e4057b1a7fa84218ccb52e91abbf096fd
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-14 09:43:00 +0000

    custom book class program

---
+++ programs/Book.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class Book {
+    public String title;
+    public String author;
+
+    public String toString(){
+        return String.format("%s by %s", this.title, this.author);
+    }
+}
