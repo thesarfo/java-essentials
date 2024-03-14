0b047fa32bba5224d474605183639a23dac9fc7a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-14 10:23:19 +0000

    modify main class to reflect the book and user class's use of getters and constructors

--- programs/HelloWorld.java
+++ programs/HelloWorld.java
@@ -1,22 +1,17 @@
 package programs;
 
-import java.time.LocalDate;
+
 
 public class HelloWorld {
     public static void main(String[] args){
-        User user = new User();
+        User user = new User("Ernest Sarfo", "2000-01-31");
 
-        user.name = "Ernest Sarfo";
-        user.birthDay = LocalDate.parse("2000-01-31");
-
-        Book book = new Book();
-
-        book.title = "Cinderella";
-        book.author = "Walt Disney";
+        Book book = new Book("Cinderella", "Walt Disney");
 
         user.borrow(book);
 
-        System.out.printf("Hello %s, you were born in %s, and he is now %d years old. \n", user.name, user.birthDay.toString(), user.age());
-        System.out.printf("%s has borrowed these books: %s", user.name, user.books.toString());
+        System.out.printf("Hello %s, you were born in %s, and he is now %d years old. \n", user.getName(), user.getBirthday(), user.age());
+
+        System.out.printf("%s has borrowed these books: %s", user.getName(), user.borrowedBooks());
     }
 }
