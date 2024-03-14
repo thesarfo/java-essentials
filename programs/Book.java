cbdb77b47a0d6a2cca7581e674201f17395646bc
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-14 10:22:49 +0000

    modify book class to use getters and constructors

--- programs/Book.java
+++ programs/Book.java
@@ -1,8 +1,21 @@
 package programs;
 
 public class Book {
-    public String title;
-    public String author;
+    private String title;
+    private String author;
+
+    Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
 
     public String toString(){
         return String.format("%s by %s", this.title, this.author);
