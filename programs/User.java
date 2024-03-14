6bf0987ecd13d7b25755d72a71acf02188505665
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-14 10:23:39 +0000

    modify user class to use getters and constructors

--- programs/User.java
+++ programs/User.java
@@ -5,9 +5,27 @@ import java.time.Period;
 import java.util.ArrayList;
 
 public class User {
-    public String name;
-    public LocalDate birthDay;
-    public ArrayList<Book> books = new ArrayList<Book>();
+    private String name;
+    private LocalDate birthDay;
+    private ArrayList<Book> books = new ArrayList<Book>();
+
+
+    // constructor method
+    User(String name, String birthDay){
+       this.name = name;
+       this.birthDay = LocalDate.parse(birthDay);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getBirthday(){
+        return this.birthDay.toString();
+    }
+
+    public String borrowedBooks(){
+        return this.books.toString();
+    }
 
     public void borrow(Book book){
         this.books.add(book);
