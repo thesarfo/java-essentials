9f230750580e704a1f838e5e12389259ecb361ed
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-16 07:55:14 +0000

    practice program

--- programs/Practice.java
+++ programs/Practice.java
@@ -2,11 +2,7 @@ package programs;
 
 public class Practice {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
-        int wage = employee.calculateWage(10);
-
-        System.out.println(wage);
+        var browser = new Browser();
+        browser.navigate("google.com");
     }
 }
