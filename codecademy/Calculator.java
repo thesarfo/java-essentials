bd471187beb4345ecd3c6f8f40d937e14ce585cf
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-18 14:25:38 +0000

    calc program

---
+++ codecademy/Calculator.java
@@ -0,0 +1,30 @@
+package codecademy;
+
+class Calculator{
+    // an oo calculator program
+    public Calculator(){
+
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int subtract(int a, int b){
+        return a - b;
+    }
+    public int multiply(int a, int b){
+        return a * b;
+    }
+    public int divide(int a, int b){
+        return a / b;
+    }
+    public int modulo(int a, int b){
+        return a % b;
+    }
+    public static void main(String[] args){
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
+    }
+}
+
