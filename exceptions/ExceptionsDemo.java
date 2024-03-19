349413f898d1139ff965714da65bc2951c5d48f4
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-19 10:26:15 +0000

    exception handling

---
+++ exceptions/ExceptionsDemo.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ExceptionsDemo {
+    public static void show(){
+        sayHello(null);
+    }
+
+    public static void sayHello(String name){
+        System.out.println(name.toUpperCase());
+    }
+}
